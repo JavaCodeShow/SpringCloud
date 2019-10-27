1adec8e768ce967e12cdbadfdbeecbdf989aa36e
jiangfeng <1546844168@qq.com>
2019-10-27 16:43:27 +0800

    initial commit
    Signed-off-by: 江峰 <jiangfeng@smarchit.com>

--- microservicecloud-consumer-hystrix-dashboard/src/main/java/com/jf/DeptConsumer_DashBoard_App.java
+++ microservicecloud-consumer-hystrix-dashboard/src/main/java/com/jf/DeptConsumer_DashBoard_App.java
@@ -2,6 +2,7 @@ package com.jf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
@@ -10,8 +11,9 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
  */
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableCircuitBreaker
 public class DeptConsumer_DashBoard_App {
-    public static void main(String[] args){
-        SpringApplication.run(DeptConsumer_DashBoard_App.class,args);
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumer_DashBoard_App.class, args);
     }
 }
