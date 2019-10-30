cf025f5dd85a9994ef2244369588324a06e6af41
江峰 <jiangfeng@smarchit.com>
2019-10-30 22:15:21 +0800

    initial commit

---
+++ microservicecloud-config-client-3355/src/main/java/com/jf/SpringBootApp_config_client_3355.java
@@ -0,0 +1,12 @@
+package com.jf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApp_config_client_3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApp_config_client_3355.class, args);
+    }
+
+}
---
+++ microservicecloud-config-client-3355/src/test/java/com/jf/microservicecloudconfigclient3355/MicroservicecloudConfigClient3355ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jf.microservicecloudconfigclient3355;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MicroservicecloudConfigClient3355ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
